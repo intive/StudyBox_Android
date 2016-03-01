e0dd5a0b0c03b6284ff41a8f547cf5540e32a35d
lkopocinski <lkopocinski@gmail.com>
2016-03-01 21:23:11 +0100

    DecksActivity created

---
+++ app/src/main/java/com/blstream/studybox/DecksActivity.java
@@ -0,0 +1,22 @@
+package com.blstream.studybox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class DecksActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+}
