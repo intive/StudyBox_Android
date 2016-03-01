b8d5b66d2b0caf287caa805f2ecbe67ddf2101c2
Olga Chróstowska <ochrostowska@gmail.com>
2016-03-01 17:21:08 +0100

    - Added two fonts listed in Visual-Language.pdf - Lato-Regular and Lato-Black - Added 4 colors defined in Visual-Language.pdf to colors.xml - Set Lato-Regular as default application font using the Calligraphy library - Added two styles StudyBoxTheme.Raspberry and StudyBoxTheme.Blue that share parent StudyBoxTheme - Added style StudyBoxTheme.BoldTextView which allows to change font to Lato-Black

---
+++ app/src/main/java/com/blstream/studybox/Application.java
@@ -0,0 +1,20 @@
+package com.blstream.studybox;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        customFontInit();
+    }
+
+    private void customFontInit() {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                        .setDefaultFontPath("fonts/Lato-Regular.ttf")
+                        .setFontAttrId(R.attr.fontPath)
+                        .build()
+        );
+    }
+}
--- app/src/main/java/com/blstream/studybox/MainActivity.java
+++ app/src/main/java/com/blstream/studybox/MainActivity.java
@@ -1,8 +1,11 @@
 package com.blstream.studybox;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Gibkie żuczki");
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
