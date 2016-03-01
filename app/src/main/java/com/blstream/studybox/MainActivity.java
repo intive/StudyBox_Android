1ec21e5a39afc4426ba711becbd1e0973f1ffb4d
Marek <macko.marek93@gmail.com>
2016-03-01 23:11:39 +0100

    Change to NavigationView

--- app/src/main/java/com/blstream/studybox/MainActivity.java
+++ app/src/main/java/com/blstream/studybox/MainActivity.java
@@ -1,25 +1,31 @@
 package com.blstream.studybox;
 
-import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.ListView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private DrawerLayout drawerLayout;
-    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "Gibkie żuczki");
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        listView = (ListView) findViewById(R.id.drawerList);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            //actionBar.setHomeAsUpIndicator();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
