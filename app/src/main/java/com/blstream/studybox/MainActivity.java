01a35114c3d4597c6b83f51c0c8af172a56b4fe9
Marek <macko.marek93@gmail.com>
2016-03-02 10:13:39 +0100

    Fix layouts

--- app/src/main/java/com/blstream/studybox/MainActivity.java
+++ app/src/main/java/com/blstream/studybox/MainActivity.java
@@ -1,6 +1,9 @@
 package com.blstream.studybox;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +30,11 @@ public class MainActivity extends AppCompatActivity {
         final ActionBar actionBar = getSupportActionBar();
 
         if (actionBar != null) {
-            //actionBar.setHomeAsUpIndicator();
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_18dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-    }
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+     }
 }
