ea237e9669f9f978b77a867cb2cee3839b584aa0
lkopocinski <lkopocinski@gmail.com>
2016-03-02 23:14:11 +0100

    RecyclerView + cardView + adapter + multiple screen support

--- app/src/main/java/com/blstream/studybox/DecksActivity.java
+++ app/src/main/java/com/blstream/studybox/DecksActivity.java
@@ -2,12 +2,23 @@ package com.blstream.studybox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import butterknife.Bind;
+import butterknife.BindInt;
 import butterknife.ButterKnife;
 
-public class DecksActivity extends AppCompatActivity {
+public class DecksActivity extends AppCompatActivity implements DecksAdapter.ClickListener {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @BindInt(R.integer.column_quantity)
+    int columnQuantity;
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
@@ -15,8 +26,27 @@ public class DecksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decks);
 
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.decks_recycler_view);
+        layoutManager = new GridLayoutManager(this, columnQuantity);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new DecksAdapter();
+        ((DecksAdapter) adapter).setOnItemClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(int position, View v) {
+        // Start test
     }
 }
