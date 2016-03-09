105edfb4f510d9c933a836efe60d3be863463e3d
Marek <macko.marek93@gmail.com>
2016-03-09 21:58:02 +0100

    Fixes to layouts for showing drawer

--- app/src/main/java/com/blstream/studybox/DecksActivity.java
+++ app/src/main/java/com/blstream/studybox/DecksActivity.java
@@ -1,9 +1,14 @@
 package com.blstream.studybox;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,7 +19,8 @@ import butterknife.Bind;
 import butterknife.BindInt;
 import butterknife.ButterKnife;
 
-public class DecksActivity extends MvpActivity<DecksView, DecksPresenter> implements DecksView, DecksAdapter.ClickListener {
+public class DecksActivity extends MvpActivity<DecksView, DecksPresenter>
+        implements DecksView, DecksAdapter.ClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     @Bind(R.id.decks_recycler_view)
     RecyclerView recyclerView;
@@ -26,6 +32,12 @@ public class DecksActivity extends MvpActivity<DecksView, DecksPresenter> implem
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    @Bind(R.id.nav_view)
+    NavigationView navigationView;
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +45,12 @@ public class DecksActivity extends MvpActivity<DecksView, DecksPresenter> implem
 
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+        drawerLayout.setDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
         loadData();
         setUpRecyclerView();
@@ -68,4 +86,31 @@ public class DecksActivity extends MvpActivity<DecksView, DecksPresenter> implem
         return new DecksPresenter();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        item.setChecked(true);
+
+        int id = item.getItemId();
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        switch (id) {
+            case R.id.my_account:
+                break;
+            case R.id.my_decks:
+                break;
+            case R.id.create_flashcard:
+                break;
+            case R.id.show_deck:
+                break;
+            case R.id.statistics:
+                break;
+            case R.id.logout:
+                break;
+        }
+
+        // Only for testing
+        Toast.makeText(this, "Selected: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
 }
