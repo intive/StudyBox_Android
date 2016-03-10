b991ce8c861c14642d3e2e95ab3234f3f5f4fc48
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2016-03-11 00:13:41 +0100

    reworked splash screen, handles rotating and back button

--- app/src/main/java/com/blstream/studybox/activities/SplashScreenActivity.java
+++ app/src/main/java/com/blstream/studybox/activities/SplashScreenActivity.java
@@ -4,47 +4,55 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
-    private boolean isSplashActive;
-    private final String BUNDLE_KEY = "SPLASH_DISPLAYED";
+    private static final int SPLASH_DISPLAY_LENGTH = 3000;
+    private static Handler   splashHandler;
+    private static Runnable  splashRunnable;
+    private static boolean   splashRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState != null) {
-            isSplashActive = savedInstanceState.getBoolean(BUNDLE_KEY);
+        if (null == splashHandler) {
+            splashHandler = new Handler();
         }
 
-        if (!isSplashActive) {
-            initializeSplashScreen(SPLASH_DISPLAY_LENGTH, DecksActivity.class);
-            isSplashActive = true;
+        if (null == splashRunnable) {
+            splashRunnable = initializeSplashRunnable(DecksActivity.class);
+        }
+
+        if (!splashRunning) {
+            initializeSplashScreen(SPLASH_DISPLAY_LENGTH);
         }
     }
 
-    /**
-     * Use this method to start a new Activity and close SplashScreen after set milliseconds
-     * @param delay amount of milliseconds to wait before finishing this activity
-     * @param destination Activity that should be started after the SplashScreen
-     */
-    private void initializeSplashScreen(int delay, final Class<?> destination) {
-        new Handler().postDelayed(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent mainIntent = new Intent(SplashScreenActivity.this, destination);
-                        SplashScreenActivity.this.startActivity(mainIntent);
-                        SplashScreenActivity.this.finish();
-                    }
-                }, delay);
+    private Runnable initializeSplashRunnable(final Class<?> destination) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, destination);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        };
+    }
+
+    private void initializeSplashScreen(int delay) {
+        splashHandler.postDelayed(splashRunnable, delay);
+        splashRunning = true;
     }
 
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putBoolean(BUNDLE_KEY, isSplashActive);
-        super.onSaveInstanceState(savedInstanceState);
+    public void onBackPressed() {
+        if (splashRunning) {
+            splashHandler.removeCallbacks(splashRunnable);
+            splashRunning = false;
+        }
+
+        super.onBackPressed();
     }
 }
