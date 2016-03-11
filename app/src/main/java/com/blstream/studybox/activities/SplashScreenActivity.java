ab71815d9525882595b1c9cb9b1e02994e1246e3
Patryk Krawczyk <patryk.krawczyk93@gmail.com>
2016-03-11 15:32:46 +0100

    update to splash screen

--- app/src/main/java/com/blstream/studybox/activities/SplashScreenActivity.java
+++ app/src/main/java/com/blstream/studybox/activities/SplashScreenActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -46,13 +45,15 @@ public class SplashScreenActivity extends AppCompatActivity {
         splashRunning = true;
     }
 
+
     @Override
-    public void onBackPressed() {
+    protected void onStop() {
         if (splashRunning) {
             splashHandler.removeCallbacks(splashRunnable);
-            splashRunning = false;
         }
-
-        super.onBackPressed();
+        splashHandler = null;
+        splashRunnable = null;
+        splashRunning = false;
+        super.onStop();
     }
 }
